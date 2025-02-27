6f53f0aa409e8af56533b294fe1a68a937185306
NdaruAjaa <120300909+NdaruAjaa@users.noreply.github.com>
2025-02-27 13:22:09 +0700

    Update

--- src/ipad54/composter/Main.php
+++ src/ipad54/composter/Main.php
@@ -4,19 +4,15 @@ declare(strict_types=1);
 
 namespace ipad54\composter;
 
-
 use ipad54\composter\block\Composter;
+use pocketmine\block\BlockFactory;
 use pocketmine\block\VanillaBlocks;
-use pocketmine\block\BlockIdentifier as BID;
-use pocketmine\data\bedrock\block\upgrade\LegacyBlockIdToStringIdMap as IDS;
-use pocketmine\item\ItemTypeIds;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase
 {
-
     protected function onLoad() : void
     {
-        VanillaBlocks::getInstance()->register(new Composter(new BID(IDS::COMPOSTER, 0, ItemIds::COMPOSTER)));
+        BlockFactory::getInstance()->registerCustomBlock(new Composter());
     }
 }

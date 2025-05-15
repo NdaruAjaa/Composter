ac60e6f68b8450a05f12c018360a079751b15ede
NdaruAjaa <120300909+NdaruAjaa@users.noreply.github.com>
2025-05-15 19:44:47 +0700

    Update Main.php

--- src/ipad54/composter/Main.php
+++ src/ipad54/composter/Main.php
@@ -5,14 +5,13 @@ declare(strict_types=1);
 namespace ipad54\composter;
 
 use ipad54\composter\block\Composter;
-use pocketmine\block\BlockFactory;
-use pocketmine\block\VanillaBlocks;
+use pocketmine\block\BlockManager;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase
 {
     protected function onLoad() : void
     {
-        BlockFactory::getInstance()->registerCustomBlock(new Composter());
+        BlockManager::getInstance()->register(new Composter());
     }
 }

3f70eac5e3ed39484e917b78d973f678bbfb2036
NdaruAjaa <120300909+NdaruAjaa@users.noreply.github.com>
2025-06-06 10:51:02 +0700

    Update Composter.php

--- src/ipad54/composter/block/Composter.php
+++ src/ipad54/composter/block/Composter.php
@@ -1,84 +1,75 @@
 <?php
 
-
 namespace ipad54\composter\block;
 
-
+use pocketmine\block\Block;
+use pocketmine\block\BlockTypeInfo;
+use pocketmine\block\Opaque;
+use pocketmine\block\utils\BlockDataSerializer;
+use pocketmine\block\utils\HorizontalFacingTrait;
+use pocketmine\item\Item;
+use pocketmine\item\ItemTypeIds;
+use pocketmine\item\VanillaItems;
+use pocketmine\math\Vector3;
 use pocketmine\network\mcpe\protocol\SpawnParticleEffectPacket;
+use pocketmine\player\Player;
 use ipad54\composter\sound\ComposteEmptySound;
 use ipad54\composter\sound\ComposteFillSound;
 use ipad54\composter\sound\ComposteFillSuccessSound;
 use ipad54\composter\sound\ComposteReadySound;
-use pocketmine\block\utils\BlockDataSerializer;
-use pocketmine\block\BlockBreakInfo;
-use pocketmine\block\BlockIdentifier;
-use pocketmine\block\BlockToolType;
-use pocketmine\block\Opaque;
-use pocketmine\item\Item;
-use pocketmine\item\ItemIds;
-use pocketmine\item\VanillaItems;
-use pocketmine\math\Vector3;
-use pocketmine\player\Player;
 
 class Composter extends Opaque
 {
+    use HorizontalFacingTrait;
 
-    /** @var int */
-    protected $fill = 0;
-    /** @var int[] */
-    protected $ingridients = [
-        ItemIds::NETHER_WART => 30,
-        ItemIds::GRASS => 30,
-        ItemIds::KELP => 30,
-        ItemIds::LEAVES => 30,
-        ItemIds::DRIED_KELP => 30,
-        ItemIds::BEETROOT_SEEDS => 30,
-        ItemIds::MELON_SEEDS => 30,
-        ItemIds::SEEDS => 30,
-        ItemIds::PUMPKIN_SEEDS => 30,
-        ItemIds::TALLGRASS => 30,
-        ItemIds::SEAGRASS => 30,
+    protected int $fill = 0;
+    
+    protected array $ingredients = [
+        ItemTypeIds::NETHER_WART => 30,
+        ItemTypeIds::GRASS => 30,
+        ItemTypeIds::KELP => 30,
+        ItemTypeIds::LEAVES => 30,
+        ItemTypeIds::DRIED_KELP => 30,
+        ItemTypeIds::BEETROOT_SEEDS => 30,
+        ItemTypeIds::MELON_SEEDS => 30,
+        ItemTypeIds::WHEAT_SEEDS => 30,
+        ItemTypeIds::PUMPKIN_SEEDS => 30,
+        ItemTypeIds::TALL_GRASS => 30,
+        ItemTypeIds::SEAGRASS => 30,
 
-        ItemIds::DRIED_KELP_BLOCK => 50,
-        ItemIds::CACTUS => 50,
-        ItemIds::MELON => 50,
-        ItemIds::SUGARCANE => 50,
+        ItemTypeIds::DRIED_KELP_BLOCK => 50,
+        ItemTypeIds::CACTUS => 50,
+        ItemTypeIds::MELON => 50,
+        ItemTypeIds::SUGARCANE => 50,
 
-        ItemIds::MELON_BLOCK => 65,
-        ItemIds::MUSHROOM_STEW => 65,
-        ItemIds::POTATO => 65,
-        ItemIds::WATER_LILY => 65,
-        ItemIds::CARROT => 65,
-        ItemIds::SEA_PICKLE => 65,
-        ItemIds::BROWN_MUSHROOM_BLOCK => 65,
-        ItemIds::RED_MUSHROOM_BLOCK => 65,
-        ItemIds::WHEAT => 65,
-        ItemIds::BEETROOT => 65,
-        ItemIds::PUMPKIN => 65,
-        ItemIds::CARVED_PUMPKIN => 65,
-        ItemIds::RED_FLOWER => 65,
-        ItemIds::YELLOW_FLOWER => 65,
-        ItemIds::APPLE => 65,
+        ItemTypeIds::MELON_BLOCK => 65,
+        ItemTypeIds::MUSHROOM_STEW => 65,
+        ItemTypeIds::POTATO => 65,
+        ItemTypeIds::WATER_LILY => 65,
+        ItemTypeIds::CARROT => 65,
+        ItemTypeIds::SEA_PICKLE => 65,
+        ItemTypeIds::BROWN_MUSHROOM_BLOCK => 65,
+        ItemTypeIds::RED_MUSHROOM_BLOCK => 65,
+        ItemTypeIds::WHEAT => 65,
+        ItemTypeIds::BEETROOT => 65,
+        ItemTypeIds::PUMPKIN => 65,
+        ItemTypeIds::CARVED_PUMPKIN => 65,
+        ItemTypeIds::RED_FLOWER => 65,
+        ItemTypeIds::YELLOW_FLOWER => 65,
+        ItemTypeIds::APPLE => 65,
 
-        ItemIds::COOKIE => 85,
-        ItemIds::BAKED_POTATO => 85,
-        ItemIds::WHEAT_BLOCK => 85,
-        ItemIds::BREAD => 85,
+        ItemTypeIds::COOKIE => 85,
+        ItemTypeIds::BAKED_POTATO => 85,
+        ItemTypeIds::HAY_BALE => 85,
+        ItemTypeIds::BREAD => 85,
 
-        ItemIds::CAKE => 100,
-        ItemIds::PUMPKIN_PIE => 100
-
+        ItemTypeIds::CAKE => 100,
+        ItemTypeIds::PUMPKIN_PIE => 100
     ];
 
-
-    public function __construct(BlockIdentifier $idInfo, ?BlockBreakInfo $breakInfo = null)
+    public function __construct(BlockTypeInfo $typeInfo)
     {
-        parent::__construct($idInfo, "Composter", $breakInfo ?? new BlockBreakInfo(0.75, BlockToolType::AXE));
-    }
-
-    public function getFuelTime(): int
-    {
-        return 300;
+        parent::__construct($typeInfo);
     }
 
     protected function writeStateToMeta(): int
@@ -101,13 +92,10 @@ class Composter extends Opaque
         $packet = new SpawnParticleEffectPacket();
         $packet->position = $position;
         $packet->particleName = "minecraft:crop_growth_emitter";
-        $recipients = $this->position->getWorld()->getViewersForPosition($this->position);
-        foreach($recipients as $player){
-            $player->getNetworkSession()->sendDataPacket($packet);
-        }
+        $this->position->getWorld()->broadcastPacketToViewers($this->position, $packet);
     }
 
-    public function onInteract(Item $item, int $face, Vector3 $clickVector, ?Player $player = null): bool
+    public function onInteract(Item $item, int $face, Vector3 $clickVector, ?Player $player = null, array &$returnedItems = []): bool
     {
         if ($this->fill >= 8) {
             $this->fill = 0;
@@ -116,33 +104,40 @@ class Composter extends Opaque
             $this->position->getWorld()->dropItem($this->position->add(0.5, 1.1, 0.5), VanillaItems::BONE_MEAL());
             return true;
         }
-        if (isset($this->ingridients[$item->getId()]) && $this->fill < 7) {
+        
+        if (isset($this->ingredients[$item->getTypeId()]) && $this->fill < 7) {
             $item->pop();
             $this->spawnParticleEffect($this->position->add(0.5, 0.5, 0.5));
+            
             if ($this->fill == 0) {
-                $this->incrimentFill(true);
+                $this->incrementFill(true);
                 return true;
             }
-            $chance = $this->ingridients[$item->getId()];
+            
+            $chance = $this->ingredients[$item->getTypeId()];
             if (mt_rand(0, 100) <= $chance) {
-                $this->incrimentFill(true);
+                $this->incrementFill(true);
                 return true;
             }
+            
             $this->position->getWorld()->addSound($this->position, new ComposteFillSound());
         }
         return true;
     }
 
-    public function incrimentFill(bool $playsound = false): bool
+    public function incrementFill(bool $playsound = false): bool
     {
         if ($this->fill >= 7) {
             return false;
         }
-        if (++$this->fill >= 7) {
+        
+        $this->fill++;
+        if ($this->fill >= 7) {
             $this->position->getWorld()->scheduleDelayedBlockUpdate($this->position, 25);
         } else {
             $this->position->getWorld()->setBlock($this->position, $this);
         }
+        
         if ($playsound) {
             $this->position->getWorld()->addSound($this->position, new ComposteFillSuccessSound());
         }
@@ -152,7 +147,7 @@ class Composter extends Opaque
     public function onScheduledUpdate(): void
     {
         if ($this->fill == 7) {
-            ++$this->fill;
+            $this->fill++;
             $this->position->getWorld()->setBlock($this->position, $this);
             $this->position->getWorld()->addSound($this->position, new ComposteReadySound());
         }
